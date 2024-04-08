f7f4b051989f2273112fbcd78ccc9343de767640
NAN24090 <noe.andrianarifidy@open-groupe.com>
2024-04-08 10:47:38 +0200

    Fixed swagger dependencies

--- georchestra-photos-obliques-facade/src/main/java/org/georchestra/photosobliques/facade/configuration/swagger/PhotosObliquesOpenApiSwaggerConfig.java
+++ georchestra-photos-obliques-facade/src/main/java/org/georchestra/photosobliques/facade/configuration/swagger/PhotosObliquesOpenApiSwaggerConfig.java
@@ -1,6 +1,6 @@
 package org.georchestra.photosobliques.facade.configuration.swagger;
 
-import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
