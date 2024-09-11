8d3f54cf33b04256e7b35cf16bf83afd8ec6964e
FNI18300 <fni18300@open-groupe.com>
2024-09-11 16:28:25 +0200

    correction TU suite suppression des schémas dans la déclaration des entités

--- georchestra-photos-obliques-service/src/test/resources/bdd/create_schema-stats.sql
+++ georchestra-photos-obliques-service/src/test/resources/bdd/create_schema-stats.sql
@@ -1,7 +1,4 @@
-CREATE SCHEMA photos_obliques_stats;
-GRANT ALL ON SCHEMA photos_obliques_stats TO consult;
-
-CREATE TABLE IF NOT EXISTS photos_obliques_stats.statistiques
+CREATE TABLE IF NOT EXISTS statistiques_photos_obliques
 (
     id bigserial NOT NULL PRIMARY KEY,
     result varchar(50),
@@ -11,4 +8,4 @@ CREATE TABLE IF NOT EXISTS photos_obliques_stats.statistiques
     who varchar(50),
     duration bigint,
     data varchar(100)
-)
+);
